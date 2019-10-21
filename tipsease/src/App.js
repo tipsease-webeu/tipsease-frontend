1026316204a16af98dc36a3b63ebb7fbdf5152b7
Pere <pere.solaclaver@gmail.com>
2019-10-21 20:28:53 +0100

    Lower state management components to app.js

--- tipsease/src/App.js
+++ tipsease/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// STATE MANAGEMENT
+
+import * as reducers from "./state/reducers";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
 
 // COMPONENTS
 
-import Counter from './components/Counter';
+import Counter from "./components/Counter";
+
+const masterReducer = combineReducers({
+  count: reducers.countReducer
+});
+
+const store = createStore(masterReducer);
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
--- tipsease/src/index.js
+++ tipsease/src/index.js
@@ -4,24 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-// STATE MANAGEMENT
-
-import * as reducers from "./state/reducers";
-import { combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
-
-const masterReducer = combineReducers({
-  count: reducers.countReducer
-});
-
-const store = createStore(masterReducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
