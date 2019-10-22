c2edb58e655fa5a0ff0af99558dae8f29dca1abe
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-22 14:08:36 +0100

    modify registration component
    In this commit, I modified the <Registration /> component and made use of the formik form library for creating forms.
    Upon submission of the registration form, an axios call is made to registration endpoint and a new user is created.

--- tipsease/src/components/Registration.jsx
+++ tipsease/src/components/Registration.jsx
@@ -8,18 +8,65 @@ import '../App.css';
 import * as actionCreators from "../state/actionCreators";
 import { connect } from 'react-redux';
 
-function Registration() {
-  return(
-    <div>
-      <form>
-        Full Name<input /><br/>
-        User Name<input /><br />
-        Password<input /><br />
-        Photo Url<input /><br />
-        <input type="submit"/>
-      </form>
-    </div>
-  )
+const initialValuesLogin = {
+  username: "",
+  password: "",
+  fullName: "",
+  photoUrl: ""
+};
+
+const userRegEndpoint = "https://build-tipsease.herokuapp.com/auth/users/register";
+
+function Registration({getCurrentUser, history}) {
+  const onRegFormSubmission = values => {
+    axios
+      .post(userRegEndpoint, {
+        fullName: values.fullname,
+        username: values.username,
+        password: values.password,
+        photoUrl: values.photourl
+      })
+      .then(res => {
+        getCurrentUser(res.data.userInfo);
+        history.push('/');
+      })
+      .catch(error => {
+        alert(error.message)
+      });
+  };
+  return (
+    <Formik
+      initialValues={initialValuesLogin}
+      onSubmit={onRegFormSubmission}
+      render={props => {
+        return (
+          <Form className="registration-form">
+            <label>
+              Full Name:
+              <Field name="fullname" type="text" />
+            </label>
+            <label>
+              password:
+              <Field name="password" type="password" />
+            </label>
+            <label>
+              User Name:
+              <Field name="username" type="text" />
+            </label>
+            <label>
+              Worker Type:
+              <Field name="text" type="text" />
+            </label>
+            <label>
+              Photo URL:
+              <Field name="photourl" type="text" />
+            </label>
+            <button type="submit">Sign Up</button>
+          </Form>
+        );
+      }}
+    />
+  );
 }
 
 export default connect(
