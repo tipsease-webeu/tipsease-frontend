86da755112fbe32e33713ed5ba64c181a96c5fbb
Pere <pere.solaclaver@gmail.com>
2019-10-22 12:24:37 +0100

    Add content to profile by pulling data from currentUser slice of state

--- tipsease/src/components/Profile.jsx
+++ tipsease/src/components/Profile.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function Profile() {
-  return <div> Hello from profile</div>;
+// STATE
+
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
+
+function Profile({currentUser}) {
+  return (
+  <div>
+      <h1>Name: {currentUser.fullName}</h1>
+      <h1>Username: {currentUser.username}</h1>
+      <h1>Password: {currentUser.password}</h1>
+  </div>
+  );
 }
+
+export default connect(
+  state => state,
+  actionCreators
+)(Profile);
