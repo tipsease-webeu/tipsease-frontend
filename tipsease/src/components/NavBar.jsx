a75f7d48b1b15d745558f96d9b30bce1db626811
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-22 13:54:23 +0100

    Add Sign Up navigation link
    In this commit, a link called sign up is added to the navigation bar.
    When clicked, this link takes a person to the registration page

--- tipsease/src/components/NavBar.jsx
+++ tipsease/src/components/NavBar.jsx
@@ -7,6 +7,7 @@ export default function NavBar({logout}) {
             Tipsease
             <NavLink to='/home'>Home</NavLink>
             <NavLink to='/profile'>Profile</NavLink>
+            <NavLink to='./register'>Sign Up</NavLink>
             <button onClick={logout}>Logout</button>
         </div>
     )
