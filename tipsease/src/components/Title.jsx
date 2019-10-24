ee208c28d1de975bfe6fd52b94f46e43f39cba49
Pere <pere.solaclaver@gmail.com>
2019-10-24 10:38:39 +0100

    Link to marketing page from app in the title of navbar

--- tipsease/src/components/Title.jsx
+++ tipsease/src/components/Title.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Title() {
-    return (
-        <div>
-            <h1>Tipsease</h1>
-            <h3>I tip, you tip, everyone tips!</h3>
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/">
+        <h1>Tipsease</h1>
+        <h3>I tip, you tip, everyone tips!</h3>
+      </Link>
+    </div>
+  );
 }
