5ed1f889b2dd1f0fe60b4e4e4680fda80e4d6a03
Pere <pere.solaclaver@gmail.com>
2019-10-24 11:39:20 +0100

    Scaffold profile page and route

---
+++ tipsease/src/components/EditProfile.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+// STATE
+
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
+
+const validationSchema = yup.object().shape({
+  fullName: yup
+    .string()
+    .required()
+    .min(2, "Too short"),
+  username: yup
+    .string()
+    .required()
+    .min(5, "Too short")
+    .max(12, "Too long")
+});
+
+function Profile({ currentUser, onEditProfile, history }) {
+  const initialValuesProfile = {
+    fullName: currentUser.fullName,
+    username: currentUser.username,
+    password: ""
+  };
+
+  const onSubmitEditProfile = (values, actions) => {
+    if (values.password === "") {
+      const { password, ...newValues } = values;
+      onEditProfile(newValues, currentUser.id);
+    } else {
+      const { ...newValues } = values;
+      onEditProfile(newValues, currentUser.id);
+    }
+    actions.resetForm();
+    history.push("/app/home");
+  };
+
+  return (
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={initialValuesProfile}
+      onSubmit={onSubmitEditProfile}
+      render={props => {
+        return (
+          <Form>
+            <label>
+              Full Name:
+              <Field name="fullName" type="text" />
+              <ErrorMessage name="fullName" component="div" />
+            </label>
+            <label>
+              Username:
+              <Field name="username" type="text" />
+              <ErrorMessage name="username" component="div" />
+            </label>
+            <label>
+              Password:
+              <Field
+                name="password"
+                type="text"
+                placeholder="Type new password"
+              />
+              <ErrorMessage name="password" component="div" />
+            </label>
+            <button>Edit profile</button>
+          </Form>
+        );
+      }}
+    />
+  );
+}
+
+export default connect(
+  state => state,
+  actionCreators
+)(Profile);
--- tipsease/src/components/Profile.jsx
+++ tipsease/src/components/Profile.jsx
@@ -1,79 +1,18 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as yup from "yup";
+import { Link } from "react-router-dom";
 
 // STATE
 
 import { connect } from "react-redux";
-import * as actionCreators from "../state/actionCreators";
 
-const validationSchema = yup.object().shape({
-  fullName: yup
-    .string()
-    .required()
-    .min(2, "Too short"),
-  username: yup
-    .string()
-    .required()
-    .min(5, "Too short")
-    .max(12, "Too long")
-});
-
-function Profile({ currentUser, onEditProfile, history }) {
-  const initialValuesProfile = {
-    fullName: currentUser.fullName,
-    username: currentUser.username,
-    password: ""
-  };
-
-  const onSubmitEditProfile = (values, actions) => {
-    if (values.password === "") {
-      const { password, ...newValues } = values;
-      onEditProfile(newValues, currentUser.id);
-    } else {
-      const { ...newValues } = values;
-      onEditProfile(newValues, currentUser.id);
-    }
-    actions.resetForm();
-    history.push("/app/home");
-  };
-
+function Profile(props) {
   return (
-    <Formik
-      validationSchema={validationSchema}
-      initialValues={initialValuesProfile}
-      onSubmit={onSubmitEditProfile}
-      render={props => {
-        return (
-          <Form>
-            <label>
-              Full Name:
-              <Field name="fullName" type="text" />
-              <ErrorMessage name="fullName" component="div" />
-            </label>
-            <label>
-              Username:
-              <Field name="username" type="text" />
-              <ErrorMessage name="username" component="div" />
-            </label>
-            <label>
-              Password:
-              <Field
-                name="password"
-                type="text"
-                placeholder="Type new password"
-              />
-              <ErrorMessage name="password" component="div" />
-            </label>
-            <button>Edit profile</button>
-          </Form>
-        );
-      }}
-    />
+    <div>
+      <div>Hellofrom profile</div>
+      <Link to="/home">back</Link>
+      <Link to="/profile/edit">Edit profile</Link>
+    </div>
   );
 }
 
-export default connect(
-  state => state,
-  actionCreators
-)(Profile);
+export default connect(state => state)(Profile);
