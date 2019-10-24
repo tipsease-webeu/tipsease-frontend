f8d47867ae68f377fdc719826cc713057944c004
Pere <pere.solaclaver@gmail.com>
2019-10-24 11:49:39 +0100

    display img profile

--- tipsease/src/components/Profile.jsx
+++ tipsease/src/components/Profile.jsx
@@ -5,15 +5,18 @@ import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 
-function Profile({currentUser}) {
+function Profile({ currentUser }) {
   return (
     <div>
       <div>
-          <ul>
-              <li>{currentUser.fullName}</li>
-              <li>{currentUser.username}</li>
-              <li>{currentUser.photoUrl}</li>
-          </ul>
+        <ul>
+          <li>{currentUser.fullName}</li>
+          <li>{currentUser.username}</li>
+          <li>{currentUser.photoUrl}</li>
+          <div>
+            <img src={currentUser.photoUrl} alt="current-user-profile-pic" />
+          </div>
+        </ul>
       </div>
       <Link to="/app/home">back</Link>
       <Link to="/app/profile/edit">Edit profile</Link>
